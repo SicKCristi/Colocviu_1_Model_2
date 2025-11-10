571d31cd19a906f56fe3d8a3d596270140e47547
SicKCristi <cristiscarlatescu20@yahoo.com>
2025-11-10 22:18:28 +0200

    final

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/PracticalTest01MainActivity.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/PracticalTest01MainActivity.kt
@@ -12,9 +12,11 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlin.toString
 
 class PracticalTest01MainActivity : AppCompatActivity() {
+    // Variabile pentru asociere cu elementele de interfata
     private lateinit var sir: EditText
     private lateinit var numar_puncte_cardinale : EditText
 
+    // Coordonate si numar
     private var coordonate=""
     private var numar=0
 
@@ -23,9 +25,14 @@ class PracticalTest01MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_practical_test01_main)
 
+        // Cerinta B1
+        // Folosim variabila sir pentru a face o asociere pentru elementul din interfata
+        // si variabile
+
         sir=findViewById<EditText>(R.id.afisare_coordonate)
         numar_puncte_cardinale=findViewById<EditText>(R.id.numar_puncte_cardinale_selectate)
 
+        // Daca se apasa butonul Nord, se adauga la sir Nord si se incrementeaza cu 1
         val apasaButonNord=findViewById<Button>(R.id.nord)
         apasaButonNord.setOnClickListener {
             coordonate+="Nord"
@@ -35,6 +42,7 @@ class PracticalTest01MainActivity : AppCompatActivity() {
             numar_puncte_cardinale.setText(numar.toString())
         }
 
+        // Daca se apasa butonul Nord, se adauga la sir Sud si se incrementeaza cu 1
         val apasaButonSud=findViewById<Button>(R.id.sud)
         apasaButonSud.setOnClickListener {
             coordonate+="Sud";
@@ -44,6 +52,7 @@ class PracticalTest01MainActivity : AppCompatActivity() {
             numar_puncte_cardinale.setText(numar.toString())
         }
 
+        // Daca se apasa butonul Nord, se adauga la sir Vest si se incrementeaza cu 1
         val apasaButonVest=findViewById<Button>(R.id.vest)
         apasaButonVest.setOnClickListener {
             coordonate+="Vest";
@@ -53,6 +62,7 @@ class PracticalTest01MainActivity : AppCompatActivity() {
             numar_puncte_cardinale.setText(numar.toString())
         }
 
+        // Daca se apasa butonul Est, se adauga la sir Nord si se incrementeaza cu 1
         val apasaButonEst=findViewById<Button>(R.id.est)
         apasaButonEst.setOnClickListener {
             coordonate+="Est";
@@ -61,6 +71,11 @@ class PracticalTest01MainActivity : AppCompatActivity() {
             sir.setText(coordonate)
             numar_puncte_cardinale.setText(numar.toString())
         }
+
+        // Cerinta B1
+
+        // Cerinta C2
+        // Se verifica ce rezultat am obtinut in urma apasarii butonului din activitatea secundara
 
         val activityResultsLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             // Dacă activitatea returnează OK
@@ -71,30 +86,44 @@ class PracticalTest01MainActivity : AppCompatActivity() {
             else if (result.resultCode == Activity.RESULT_CANCELED) {
                 Toast.makeText(this, "The activity returned with result CANCELED", Toast.LENGTH_LONG).show()
             }
+
+            // Vom reseta si elementele
+            coordonate=""
+            numar=0
+            sir.setText("")
+            numar_puncte_cardinale.setText("0")
         }
 
         // La click pe buton, pornește activitatea secundară și trimite valorile din input1 și input2
         val navigateToSecondaryActivityButton = findViewById<Button>(R.id.activitate_secundara)
         navigateToSecondaryActivityButton.setOnClickListener {
             val intent = Intent(this, PracticalTest01SecondaryActivity::class.java)
-            intent.putExtra("coordonate", Integer.valueOf(sir.text.toString()))
+            intent.putExtra("coordonate", sir.text.toString())
             activityResultsLauncher.launch(intent)
         }
+
+        // Cerinta C2
     }
 
+    // Cerinta B2
+
+    // Vom salva valorile in elementele de interfata cu celelalte variabile din clasa
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString("numar_puncte_cardinale_selectate",numar.toString())
         outState.putString("coordonate",coordonate)
     }
 
+    // Vom pune inapoi valori in campurile din interfata daca acestea nu sunt 0
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        if (savedInstanceState.containsKey("numar_puncte_cardinale") && savedInstanceState.containsKey("coordonate")){
-            numar_puncte_cardinale.setText(savedInstanceState.getString("numar_puncte_carinale"))
+        if (savedInstanceState.containsKey("numar_puncte_cardinale_selectate") && savedInstanceState.containsKey("coordonate")) {
+            numar_puncte_cardinale.setText(savedInstanceState.getString("numar_puncte_cardinale_selectate"))
             sir.setText(savedInstanceState.getString("coordonate"))
-            numar=Integer.valueOf(numar_puncte_cardinale.toString())
-            coordonate=sir.toString()
+            numar = numar_puncte_cardinale.text.toString().toInt()
+            coordonate = sir.text.toString()
         }
     }
+
+    // Cerinta B2
     }
--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/PracticalTest01SecondaryActivity.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/PracticalTest01SecondaryActivity.kt
@@ -1,3 +1,8 @@
+// Cerinta C1
+// Se implementeaza activitatea secundara si interfata pentru aceasta
+
+package ro.pub.cs.systems.eim.practicaltest01
+
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -8,6 +13,7 @@ import ro.pub.cs.systems.eim.practicaltest01.R
 
 class PracticalTest01SecondaryActivity : AppCompatActivity() {
 
+    // Variabila care asocieaza cu elementul de interfata
     private lateinit var coordonate: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +21,12 @@ class PracticalTest01SecondaryActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_practical_test_01_secondary)
 
+        // Asociere + valoarea din intent + setare in campul de interfata din activitatea secundara
         coordonate = findViewById<TextView>(R.id.coordonatele)
         val input1 = intent.getStringExtra("coordonate")
+        coordonate.setText(input1)
 
-        // Varaiabila retine asocierea cu butonul ok
+        // Varaiabila retine asocierea cu butonul register
         // Se termina activitatea si mesajul este ok
         val okButton = findViewById<Button>(R.id.butonRegister)
         okButton.setOnClickListener {
